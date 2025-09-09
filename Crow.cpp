8a111be5ab56c04edbb780f28b7db113278ffa1a
Rodrigo Estrela <160602751+estrela801@users.noreply.github.com>
2025-09-09 14:49:27 +0000

    Criando configuracoes basicas de uma api com o Crow

---
+++ Crow.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include"calls.h" // chamando o arquivo de cabeçalho com as declarações
+#include "crow.h"
+using namespace std;
+
+int main(){
+    crow::SimpleApp app; // criando uma instância do aplicativo
+    CROW_ROUTE(app, "/")([]() {
+        return "Hello, World!";
+    });
+    app.port(18080).multithreaded().run(); // iniciando o servidor na porta 18080
+}
